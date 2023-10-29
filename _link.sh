cf3ce02192211648016f7ca6cc8ac34083fdab3d
Takuto <tiharu717@gmail.com>
2023-10-29 21:30:15 +0900

    .configフォルダがなかったら作成するようにした

--- _link.sh
+++ _link.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+# フォルダの作成
+if [ ! -e $HOME/.config ];then
+	mkdir $HOME/.config
+fi
 # シンボリックリンクの作成
 DOT_FILES=".zshrc .zprofile .oh-my-zsh .tmux.conf"
 for file in $DOT_FILES
 do
     ln -sf `pwd`/$file ~
 done
+
+ln -sf `pwd`/"nvim" ~/.config/
 
