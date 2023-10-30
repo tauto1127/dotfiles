f8bbed4d92cb77728a1b6ad81031f91954702dbf
Takuto <tiharu717@gmail.com>
2023-10-30 10:49:31 +0900

    nvimのインストール修正、シェルの変更要請メッセージ追加

--- aptInstall.sh
+++ aptInstall.sh
@@ -1,3 +1,4 @@
+# ZSH INSTALL
 sudo apt install zsh -y
 chsh -s $(which zsh)
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
@@ -7,6 +8,8 @@ cd fonts
 cd ..
 
 # NVIM INSTALL
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract
 ./squashfs-root/AppRun --version
 sudo mv squashfs-root /
@@ -14,5 +17,6 @@ sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 
 sudo apt install tmux -y
 
-echo "シェルをzshに変更"
-chsh -s /bin/zsh
+echo "Please run\n"
+echo "chsh -s /bin/zsh\n"
+echo "and re login"
