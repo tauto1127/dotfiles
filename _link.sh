87f6d1c01805feef35788403bb07911d2f3372b0
Takuto <tiharu717@gmail.com>
2023-12-29 03:46:25 +0900

    oh-my-zshのリポジトリ入れないですむようにしたし，powerlevel10kの設定も同期できるようにした．

--- _link.sh
+++ _link.sh
@@ -5,7 +5,7 @@ if [ ! -e $HOME/.config ];then
 	mkdir $HOME/.config
 fi
 # シンボリックリンクの作成
-DOT_FILES=".zshrc .zprofile .tmux.conf"
+DOT_FILES=".zshrc .zprofile .tmux.conf .p10k.zsh"
 for file in $DOT_FILES
 do
     ln -sf `pwd`/$file ~
