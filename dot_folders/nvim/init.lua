f697d0dbd5a79c79ef4016ea1c24a10d4ac745d2
takuto <tiharu717@gmail.com>
2024-07-01 10:17:30 +0900

    toggletermの設定と，nvim-treesitterがバグるため無効にした

--- dot_folders/nvim/init.lua
+++ dot_folders/nvim/init.lua
@@ -39,7 +39,7 @@ if nocode then
   table.insert(plugins, require('toggleterm_plugin'))
   table.insert(plugins, require('nvim-lsp-file-operations_plugin'))
   table.insert(plugins, 'christoomey/vim-tmux-navigator')
-  table.insert(plugins, 'nvim-treesitter/nvim-treesitter')
+  --table.insert(plugins, 'nvim-treesitter/nvim-treesitter')
 
 	--tab
 	table.insert(plugins, {
@@ -234,31 +234,31 @@ null_ls.setup({
 			end,
 		},
   }
-  require'nvim-treesitter.configs'.setup {
-    -- A list of parser names, or "all" (the five listed parsers should always be installed)
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+  --require'nvim-treesitter.configs'.setup {
+  --  -- A list of parser names, or "all" (the five listed parsers should always be installed)
+  --  ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
 
-    -- Install parsers synchronously (only applied to `ensure_installed`)
-    sync_install = false,
+  --  -- Install parsers synchronously (only applied to `ensure_installed`)
+  --  sync_install = false,
 
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-    auto_install = true,
+  --  -- Automatically install missing parsers when entering buffer
+  --  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  --  auto_install = true,
 
-    -- List of parsers to ignore installing (or "all")
-    ignore_install = { "javascript" },
-    highlight = {
-      enable = true,
-      -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-      disable = function(lang, buf)
-          local max_filesize = 100 * 1024 -- 100 KB
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_filesize then
-              return true
-          end
-      end,
-    },
-  }
+  --  -- List of parsers to ignore installing (or "all")
+  --  ignore_install = { "javascript" },
+  --  highlight = {
+  --    enable = true,
+  --    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+  --    disable = function(lang, buf)
+  --        local max_filesize = 100 * 1024 -- 100 KB
+  --        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+  --        if ok and stats and stats.size > max_filesize then
+  --            return true
+  --        end
+  --    end,
+  --  },
+  --}
 
   --require('coc_plugin')
   require("nvim-tree").setup { -- BEGIN_DEFAULT_OPTS
@@ -526,7 +526,7 @@ if nocode then
     --vim.nnoremap <C-t> :NvimTreeToggle <CR>
   --crで囲むとコマンドとして認識される
   --silentをオンにすると，エラーなどが表示されなくなる
-  vim.api.nvim_set_keymap('n', '<C-t>', ':NvimTreeToggle<cr>', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('n', '<C-t>', '<cmd>NvimTreeToggle<cr>', { noremap = true, silent = true })
   vim.api.nvim_set_keymap('t', 'fj', '<C-\\><C-n>', { noremap = true, silent = true })
   --telescope vscodeのようなファイル探索
   vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files hidden=true<cr>', {noremap = true, silent=true})
@@ -556,6 +556,8 @@ if nocode then
 		direction = "float",
 		hidden = true
 	})
+
+	vim.g.termguicolors = true
 
 	function _lazygit_toggle()
 		lazygit:toggle()
