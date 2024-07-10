4549892b545706fdb3cf2d2901bbc476a33ead1a
takuto <tiharu717@gmail.com>
2024-07-10 11:54:38 +0900

    add wezterm

--- _link.sh
+++ _link.sh
@@ -6,17 +6,25 @@ if [ ! -e $HOME/.config ];then
 fi
 # シンボリックリンクの作成
 DOT_FILES=".zshrc .zprofile .tmux.conf .p10k.zsh .skhdrc .yabairc"
-# フォルダー
-DOT_FOLDERS="nvim karabiner iterm2 yabai skhd"
+# .configフォルダー
+CONFIGDOT_FOLDERS="nvim karabiner iterm2 yabai skhd wezterm"
+
+#~/folder
+DOT_FOLDERS=".hammerspoon"
 
 for file in $DOT_FILES
 do
     ln -sf `pwd`/$file ~
 done
 
-for folder in $DOT_FOLDERS
+for folder in $CONFIGDOT_FOLDERS
 do
 	ln -sf `pwd`/dot_folders/$folder ~/.config/
+done
+
+for folder in $DOT_FOLDERS
+do
+	ln -sf `pwd`/dot_folders/$folder ~
 done
 
 #ln -sf `pwd`/"nvim" ~/.config/
