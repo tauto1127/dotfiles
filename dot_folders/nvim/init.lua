67290b467bf18b9e5b56936f86d5b40dcf8020b1
takuto <tiharu717@gmail.com>
2024-08-19 11:12:45 +0900

    feat: Enable highlighting of color variables in lsp for flutter >= 2.10
    This commit modifies the `flutter-tools_plugin.lua` file to enable the highlighting of color variables in the language server protocol (lsp) for Flutter versions 2.10 and above. The `enabled` and `background` options are set to `true`, and the `foreground` option is set to `true`. This change improves the visibility and usability of color variables in the code.

--- dot_folders/nvim/init.lua
+++ dot_folders/nvim/init.lua
@@ -361,113 +361,113 @@ if nocode then
 			end
 		end,
 	})
-	require("obsidian").setup({
-		--Obsidianの設定
-		workspaces = {
-			{
-				name = "main",
-				path = "/Users/takuto/Library/Mobile Documents/iCloud~md~obsidian/Documents/main",
-			},
-		},
-		daily_notes = {
-			folder = "extend/DairyNote",
-			template = "extend/template/DairyNote/DairyNoteTemplate.md",
-		},
-		completion = {
-			nvim_cmp = true,
-			min_chars = 2,
-		},
-		mappings = {
-			-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-			["gf"] = {
-				action = function()
-					return require("obsidian").util.gf_passthrough()
-				end,
-				opts = { noremap = false, expr = true, buffer = true },
-			},
-			-- Toggle check-boxes.
-			["<leader>ch"] = {
-				action = function()
-					return require("obsidian").util.toggle_checkbox()
-				end,
-				opts = { buffer = true },
-			},
-			-- Smart action depending on context, either follow link or toggle checkbox.
-			["<cr>"] = {
-				action = function()
-					return require("obsidian").util.smart_action()
-				end,
-				opts = { buffer = true, expr = true },
-			},
-		},
-		-- Optional, customize how note file names are generated given the ID, target directory, and title.
-		---@param spec { id: string, dir: obsidian.Path, title: string|?  }
-		---@return string|obsidian.Path The full path to the new note.
-		note_path_func = function(spec)
-			-- This is equivalent to the default behavior.
-			local path = spec.dir / tostring(spec.title)
-			return path:with_suffix(".md")
-		end,
-		new_notes_location = "current_dir",
-		-- Optional, alternatively you can customize the frontmatter data.
-		---@return table
-		note_frontmatter_func = function(note)
-			-- Add the title of the note as an alias.
-			if note.title then
-				note:add_alias(note.title)
-			end
-			local d = os.date("*t")
+	--require("obsidian").setup({
+	--	--Obsidianの設定
+	--	workspaces = {
+	--		{
+	--			name = "main",
+	--			path = "/Users/takuto/Library/Mobile Documents/iCloud~md~obsidian/Documents/main",
+	--		},
+	--	},
+	--	daily_notes = {
+	--		folder = "extend/DairyNote",
+	--		template = "extend/template/DairyNote/DairyNoteTemplate.md",
+	--	},
+	--	completion = {
+	--		nvim_cmp = true,
+	--		min_chars = 2,
+	--	},
+	--	mappings = {
+	--		-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+	--		["gf"] = {
+	--			action = function()
+	--				return require("obsidian").util.gf_passthrough()
+	--			end,
+	--			opts = { noremap = false, expr = true, buffer = true },
+	--		},
+	--		-- Toggle check-boxes.
+	--		["<leader>ch"] = {
+	--			action = function()
+	--				return require("obsidian").util.toggle_checkbox()
+	--			end,
+	--			opts = { buffer = true },
+	--		},
+	--		-- Smart action depending on context, either follow link or toggle checkbox.
+	--		["<cr>"] = {
+	--			action = function()
+	--				return require("obsidian").util.smart_action()
+	--			end,
+	--			opts = { buffer = true, expr = true },
+	--		},
+	--	},
+	--	-- Optional, customize how note file names are generated given the ID, target directory, and title.
+	--	---@param spec { id: string, dir: obsidian.Path, title: string|?  }
+	--	---@return string|obsidian.Path The full path to the new note.
+	--	note_path_func = function(spec)
+	--		-- This is equivalent to the default behavior.
+	--		local path = spec.dir / tostring(spec.title)
+	--		return path:with_suffix(".md")
+	--	end,
+	--	new_notes_location = "current_dir",
+	--	-- Optional, alternatively you can customize the frontmatter data.
+	--	---@return table
+	--	note_frontmatter_func = function(note)
+	--		-- Add the title of the note as an alias.
+	--		if note.title then
+	--			note:add_alias(note.title)
+	--		end
+	--		local d = os.date("*t")
 
-			local out = {
-				id = note.id,
-				aliases = note.aliases,
-				tags = note.tags,
-				created = d["year"] .. "-" .. d["month"] .. "-" .. d["day"] .. " " .. d["hour"] .. ":" .. d["min"],
-			}
+	--		local out = {
+	--			id = note.id,
+	--			aliases = note.aliases,
+	--			tags = note.tags,
+	--			created = d["year"] .. "-" .. d["month"] .. "-" .. d["day"] .. " " .. d["hour"] .. ":" .. d["min"],
+	--		}
 
-			-- `note.metadata` contains any manually added fields in the frontmatter.
-			-- So here we just make sure those fields are kept in the frontmatter.
-			if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-				for k, v in pairs(note.metadata) do
-					out[k] = v
-				end
-			end
+	--		-- `note.metadata` contains any manually added fields in the frontmatter.
+	--		-- So here we just make sure those fields are kept in the frontmatter.
+	--		if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+	--			for k, v in pairs(note.metadata) do
+	--				out[k] = v
+	--			end
+	--		end
 
-			return out
-		end,
-		templates = {
-			folder = "extend/template",
-		},
-		picker = {
-			name = "telescope.nvim",
-			mappings = {
-				new = "<C-x>",
-				insert_link = "<C-l>",
-			},
-		},
-		sort_by = "modified",
-		sort_reversed = true,
-		-- Optional, boolean or a function that takes a filename and returns a boolean.
-		-- `true` indicates that you don't want obsidian.nvim to manage frontmatter.
-		disable_frontmatter = false,
-		-- Specify how to handle attachments.
-		attachments = {
-			-- The default folder to place images in via `:ObsidianPasteImg`.
-			-- If this is a relative path it will be interpreted as relative to the vault root.
-			-- You can always override this per image by passing a full path to the command instead of just a filenameextend/files.
-			img_folder = "extend/files", -- This is the default
-			-- A function that determines the text to insert in the note when pasting an image.
-			-- It takes two arguments, the `obsidian.Client` and an `obsidian.Path` to the image file.
-			-- This is the default implementation.
-			---@param client obsidian.Client
-			---@param path obsidian.Path the absolute path to the image file
-			---@return string
-			img_text_func = function(client, path)
-				path = client:vault_relative_path(path) or path
-				return string.format("![%s](%s)", path.name, path)
-			end,
-		},
-	})
+	--		return out
+	--	end,
+	--	templates = {
+	--		folder = "extend/template",
+	--	},
+	--	picker = {
+	--		name = "telescope.nvim",
+	--		mappings = {
+	--			new = "<C-x>",
+	--			insert_link = "<C-l>",
+	--		},
+	--	},
+	--	sort_by = "modified",
+	--	sort_reversed = true,
+	--	-- Optional, boolean or a function that takes a filename and returns a boolean.
+	--	-- `true` indicates that you don't want obsidian.nvim to manage frontmatter.
+	--	disable_frontmatter = false,
+	--	-- Specify how to handle attachments.
+	--	attachments = {
+	--		-- The default folder to place images in via `:ObsidianPasteImg`.
+	--		-- If this is a relative path it will be interpreted as relative to the vault root.
+	--		-- You can always override this per image by passing a full path to the command instead of just a filenameextend/files.
+	--		img_folder = "extend/files", -- This is the default
+	--		-- A function that determines the text to insert in the note when pasting an image.
+	--		-- It takes two arguments, the `obsidian.Client` and an `obsidian.Path` to the image file.
+	--		-- This is the default implementation.
+	--		---@param client obsidian.Client
+	--		---@param path obsidian.Path the absolute path to the image file
+	--		---@return string
+	--		img_text_func = function(client, path)
+	--			path = client:vault_relative_path(path) or path
+	--			return string.format("![%s](%s)", path.name, path)
+	--		end,
+	--	},
+	--})
 
 	require("nvim-treesitter.configs").setup({
 		-- A list of parser names, or "all" (the five listed parsers should always be installed)
@@ -479,6 +479,9 @@ if nocode then
 		-- Automatically install missing parsers when entering buffer
 		-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
 		auto_install = true,
+		indent = {
+			enable = true,
+		},
 
 		-- List of parsers to ignore installing (or "all")
 		ignore_install = { "javascript" },
@@ -841,7 +844,7 @@ if nocode then
 		lazygit:toggle()
 	end
 
-	vim.api.nvim_set_keymap("n", "<leader> lg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+	vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
 end
 -- バッファの内容全体を使って Copilot とチャットする
 function CopilotChatBuffer()
@@ -857,8 +860,6 @@ vim.o.clipboard = vim.o.clipboard .. "unnamedplus"
 --インデント
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.autoindent = true
-vim.opt.smartindent = true
 
 --conceallevel
 --obsidian-nvimで指定された
--- dot_folders/nvim/lua/flutter-tools_plugin.lua
+++ dot_folders/nvim/lua/flutter-tools_plugin.lua
@@ -62,10 +62,10 @@ return {
 			},
 			lsp = {
 				color = { -- show the derived colours for dart variables
-					enabled = false, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
-					background = false, -- highlight the background
+					enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+					background = true, -- highlight the background
 					background_color = nil, -- required, when background is transparent (i.e. background_color = { r = 19, g = 17, b = 24},)
-					foreground = false, -- highlight the foreground
+					foreground = true, -- highlight the foreground
 					virtual_text = true, -- show the highlight using virtual text
 					virtual_text_str = "■", -- the virtual text character to highlight
 				},
