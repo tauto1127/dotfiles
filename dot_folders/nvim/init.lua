17234062a596a140f5ea22684fb99e1d3a04fe7d
takuto <tiharu717@gmail.com>
2024-08-22 03:29:18 +0900

    nvim-treesitter復活！

--- dot_folders/nvim/init.lua
+++ dot_folders/nvim/init.lua
@@ -32,32 +32,31 @@ local plugins = {
 }
 
 if nocode then
-	table.insert(plugins, "nvim-treesitter/nvim-treesitter")
-	--table.insert(plugins, {
-	--	"nvim-treesitter/nvim-treesitter",
-	--	build = ":TSUpdate",
-	--	config = function()
-	--		local configs = require("nvim-treesitter.configs")
-	--		configs.setup({
-	--			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-	--			sync_install = false,
-	--			highlight = { enable = true },
-	--			indent = { enable = true },
-	--			auto_install = true,
-	--			ignore_install = { "javascript" },
-	--			highlight = {
-	--				enable = true,
-	--				disable = function(lang, buf)
-	--					local max_filesize = 100 * 1024 -- 100 KB
-	--					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-	--					if ok and stats and stats.size > max_filesize then
-	--						return true
-	--					end
-	--				end,
-	--			},
-	--		})
-	--	end,
-	--})
+	table.insert(plugins, {
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+			configs.setup({
+				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+				auto_install = true,
+				ignore_install = { "javascript" },
+				highlight = {
+					enable = true,
+					disable = function(lang, buf)
+						local max_filesize = 100 * 1024 -- 100 KB
+						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+						if ok and stats and stats.size > max_filesize then
+							return true
+						end
+					end,
+				},
+			})
+		end,
+	})
 
 	table.insert(plugins, require("copilotchat"))
 	table.insert(plugins, require("trouble_plugin"))
