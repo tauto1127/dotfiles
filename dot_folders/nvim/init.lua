c7fc24e92b1dfc34a0bf0f67bcb8b2029fa76934
takuto <tiharu717@gmail.com>
2024-08-21 14:51:54 +0900

    fix obsidian frontmatter settings

--- dot_folders/nvim/init.lua
+++ dot_folders/nvim/init.lua
@@ -317,7 +317,9 @@ if nocode then
 			},
 		},
 	})
+	require("lspconfig").eslint.setup({})
 	require("lspconfig").csharp_ls.setup({})
+	require("lspconfig").tsserver.setup({})
 	--require("lspconfig").dartls.setup({
 	--	init_options = {
 	--		closingLabels = true,
@@ -341,8 +343,6 @@ if nocode then
 			null_ls.builtins.formatting.shfmt,
 			--null_ls.builtins.diagnostics.shellcheck,
 			--null_ls.builtins.code_actions.shellcheck,
-			null_ls.builtins.formatting.prettier,
-
 			null_ls.builtins.formatting.dart_format,
 		},
 		-- you can reuse a shared lspconfig on_attach callback here
@@ -361,113 +361,105 @@ if nocode then
 			end
 		end,
 	})
-	--require("obsidian").setup({
-	--	--Obsidianの設定
-	--	workspaces = {
-	--		{
-	--			name = "main",
-	--			path = "/Users/takuto/Library/Mobile Documents/iCloud~md~obsidian/Documents/main",
-	--		},
-	--	},
-	--	daily_notes = {
-	--		folder = "extend/DairyNote",
-	--		template = "extend/template/DairyNote/DairyNoteTemplate.md",
-	--	},
-	--	completion = {
-	--		nvim_cmp = true,
-	--		min_chars = 2,
-	--	},
-	--	mappings = {
-	--		-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-	--		["gf"] = {
-	--			action = function()
-	--				return require("obsidian").util.gf_passthrough()
-	--			end,
-	--			opts = { noremap = false, expr = true, buffer = true },
-	--		},
-	--		-- Toggle check-boxes.
-	--		["<leader>ch"] = {
-	--			action = function()
-	--				return require("obsidian").util.toggle_checkbox()
-	--			end,
-	--			opts = { buffer = true },
-	--		},
-	--		-- Smart action depending on context, either follow link or toggle checkbox.
-	--		["<cr>"] = {
-	--			action = function()
-	--				return require("obsidian").util.smart_action()
-	--			end,
-	--			opts = { buffer = true, expr = true },
-	--		},
-	--	},
-	--	-- Optional, customize how note file names are generated given the ID, target directory, and title.
-	--	---@param spec { id: string, dir: obsidian.Path, title: string|?  }
-	--	---@return string|obsidian.Path The full path to the new note.
-	--	note_path_func = function(spec)
-	--		-- This is equivalent to the default behavior.
-	--		local path = spec.dir / tostring(spec.title)
-	--		return path:with_suffix(".md")
-	--	end,
-	--	new_notes_location = "current_dir",
-	--	-- Optional, alternatively you can customize the frontmatter data.
-	--	---@return table
-	--	note_frontmatter_func = function(note)
-	--		-- Add the title of the note as an alias.
-	--		if note.title then
-	--			note:add_alias(note.title)
-	--		end
-	--		local d = os.date("*t")
+	null_ls.builtins.code_actions.gitsigns.with({
+		disabled_filetypes = { "typescript", "ts" },
+	})
+	require("obsidian").setup({
+		--Obsidianの設定
+		workspaces = {
+			{
+				name = "main",
+				path = "/Users/takuto/Nextcloud/extendApps/Obsidian/main",
+			},
+		},
+		daily_notes = {
+			folder = "extend/DairyNote",
+			template = "extend/template/DairyNote/DairyNoteTemplate.md",
+		},
+		completion = {
+			nvim_cmp = true,
+			min_chars = 2,
+		},
+		mappings = {
+			-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+			["gf"] = {
+				action = function()
+					return require("obsidian").util.gf_passthrough()
+				end,
+				opts = { noremap = false, expr = true, buffer = true },
+			},
+			-- Smart action depending on context, either follow link or toggle checkbox.
+			["<cr>"] = {
+				action = function()
+					return require("obsidian").util.smart_action()
+				end,
+				opts = { buffer = true, expr = true },
+			},
+		},
+		-- Optional, customize how note file names are generated given the ID, target directory, and title.
+		---@param spec { id: string, dir: obsidian.Path, title: string|?  }
+		---@return string|obsidian.Path The full path to the new note.
+		note_path_func = function(spec)
+			-- This is equivalent to the default behavior.
+			local path = spec.dir / tostring(spec.title)
+			return path:with_suffix(".md")
+		end,
+		new_notes_location = "current_dir",
+		-- Optional, alternatively you can customize the frontmatter data.
+		---@return table
+		note_frontmatter_func = function(note)
+			local d = os.date("*t")
 
-	--		local out = {
-	--			id = note.id,
-	--			aliases = note.aliases,
-	--			tags = note.tags,
-	--			created = d["year"] .. "-" .. d["month"] .. "-" .. d["day"] .. " " .. d["hour"] .. ":" .. d["min"],
-	--		}
+			local out = {
+				--id = note.id,
+				aliases = note.aliases,
+				tags = note.tags,
+				created = d["year"] .. "-" .. d["month"] .. "-" .. d["day"] .. " " .. d["hour"] .. ":" .. d["min"],
+			}
 
-	--		-- `note.metadata` contains any manually added fields in the frontmatter.
-	--		-- So here we just make sure those fields are kept in the frontmatter.
-	--		if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-	--			for k, v in pairs(note.metadata) do
-	--				out[k] = v
-	--			end
-	--		end
+			-- `note.metadata` contains any manually added fields in the frontmatter.
+			-- So here we just make sure those fields are kept in the frontmatter.
+			if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+				for k, v in pairs(note.metadata) do
+					out[k] = v
+				end
+			end
 
-	--		return out
-	--	end,
-	--	templates = {
-	--		folder = "extend/template",
-	--	},
-	--	picker = {
-	--		name = "telescope.nvim",
-	--		mappings = {
-	--			new = "<C-x>",
-	--			insert_link = "<C-l>",
-	--		},
-	--	},
-	--	sort_by = "modified",
-	--	sort_reversed = true,
-	--	-- Optional, boolean or a function that takes a filename and returns a boolean.
-	--	-- `true` indicates that you don't want obsidian.nvim to manage frontmatter.
-	--	disable_frontmatter = false,
-	--	-- Specify how to handle attachments.
-	--	attachments = {
-	--		-- The default folder to place images in via `:ObsidianPasteImg`.
-	--		-- If this is a relative path it will be interpreted as relative to the vault root.
-	--		-- You can always override this per image by passing a full path to the command instead of just a filenameextend/files.
-	--		img_folder = "extend/files", -- This is the default
-	--		-- A function that determines the text to insert in the note when pasting an image.
-	--		-- It takes two arguments, the `obsidian.Client` and an `obsidian.Path` to the image file.
-	--		-- This is the default implementation.
-	--		---@param client obsidian.Client
-	--		---@param path obsidian.Path the absolute path to the image file
-	--		---@return string
-	--		img_text_func = function(client, path)
-	--			path = client:vault_relative_path(path) or path
-	--			return string.format("![%s](%s)", path.name, path)
-	--		end,
-	--	},
-	--})
+			return out
+		end,
+		templates = {
+			folder = "extend/template",
+		},
+		picker = {
+			name = "telescope.nvim",
+			mappings = {
+				new = "<C-x>",
+				insert_link = "<C-l>",
+			},
+		},
+		sort_by = "modified",
+		sort_reversed = true,
+		-- Optional, boolean or a function that takes a filename and returns a boolean.
+		-- `true` indicates that you don't want obsidian.nvim to manage frontmatter.
+		disable_frontmatter = false,
+		-- Specify how to handle attachments.
+		attachments = {
+			-- The default folder to place images in via `:ObsidianPasteImg`.
+			-- If this is a relative path it will be interpreted as relative to the vault root.
+			-- You can always override this per image by passing a full path to the command instead of just a filenameextend/files.
+			img_folder = "extend/files", -- This is the default
+			-- A function that determines the text to insert in the note when pasting an image.
+			-- It takes two arguments, the `obsidian.Client` and an `obsidian.Path` to the image file.
+			-- This is the default implementation.
+			---@param client obsidian.Client
+			---@param path obsidian.Path the absolute path to the image file
+			---@return string
+			img_text_func = function(client, path)
+				path = client:vault_relative_path(path) or path
+				return string.format("![%s](%s)", path.name, path)
+			end,
+		},
+	})
 
 	require("nvim-treesitter.configs").setup({
 		-- A list of parser names, or "all" (the five listed parsers should always be installed)
