ca157f6f280768394c7dbd590ac1793c5cf6a0a9
takuto1127 <tiharu717@gmail.com>
2024-11-18 13:11:31 +0900

    obsidianをctrl2回で開くようにした

--- dot_folders/.hammerspoon/init.lua
+++ dot_folders/.hammerspoon/init.lua
@@ -30,8 +30,23 @@ local open_ticktick = function()
   end
 end
 
+local open_obsidian = function()
+  local appName = "Obsidian"
+  local app = hs.application.get(appName)
+
+  if app == nil or app:isHidden() then
+    hs.application.launchOrFocus(appName)
+  else
+    if app:isFrontmost() then
+      app:hide()
+    else
+      app:activate()
+    end
+  end
+end
+
 double_press.timeFrame = 0.3
-double_press.action = open_wezterm
+double_press.action = open_obsidian
 
 -- ticktick
 hs.hotkey.bind({ "ctrl", "cmd" }, "t", function()
