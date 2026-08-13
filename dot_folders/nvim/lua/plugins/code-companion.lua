a1c28f96cee73621e8b210aecfc83ed480f664b6
takuto1127 <tiharu717@gmail.com>
2026-08-13 16:22:53 +0900

    feat(nvim): codecompanion-history.nvimを追加し過去会話履歴の保存・検索に対応

--- dot_folders/nvim/lua/plugins/code-companion.lua
+++ dot_folders/nvim/lua/plugins/code-companion.lua
@@ -4,6 +4,7 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
+    "ravitemer/codecompanion-history.nvim",
   },
   config = function()
     require("codecompanion").setup({
@@ -29,8 +30,11 @@ return {
         language = "ja",
       },
     })
+
+    -- キーバインド設定
     vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
     vim.api.nvim_set_keymap("n", "<C-c>", ":CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
     vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+    vim.keymap.set("n", "<leader>ch", "<cmd>CodeCompanionHistory<cr>", { desc = "CodeCompanion 履歴一覧", noremap = true, silent = true })
   end,
 }
