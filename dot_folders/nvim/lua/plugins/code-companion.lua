823e9803f1b99a4cb07dfefb4b7c8df5afcd1099
takuto1127 <tiharu717@gmail.com>
2026-08-13 16:33:18 +0900

    fix(nvim): codecompanion-historyをCodeCompanion拡張として正しく連携しCtrl+hにバインド

--- dot_folders/nvim/lua/plugins/code-companion.lua
+++ dot_folders/nvim/lua/plugins/code-companion.lua
@@ -8,6 +8,11 @@ return {
   },
   config = function()
     require("codecompanion").setup({
+      extensions = {
+        history = {
+          enabled = true,
+        },
+      },
       adapters = {
         acp = {
           opencode = function()
@@ -35,6 +40,14 @@ return {
     vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
     vim.api.nvim_set_keymap("n", "<C-c>", ":CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
     vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
-    vim.keymap.set("n", "<leader>ch", "<cmd>CodeCompanionHistory<cr>", { desc = "CodeCompanion 履歴一覧", noremap = true, silent = true })
+    -- Ctrl + h で過去の会話履歴一覧を表示・検索・復元
+    vim.keymap.set("n", "<C-h>", function()
+      local history = require("codecompanion").extensions.history
+      if history and history.browse then
+        history.browse()
+      else
+        vim.cmd("CodeCompanionActions")
+      end
+    end, { desc = "CodeCompanion 過去会話履歴一覧", noremap = true, silent = true })
   end,
 }
